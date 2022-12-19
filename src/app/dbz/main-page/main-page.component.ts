277e7a2aaf274a316636b9b9f0f71527f8486d35
Andres Ojeda <carlosandresoj@gmail.com>
2022-12-19 17:05:32 -0500

    Agregar components

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,17 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
-
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
+
 export class MainPageComponent {
-
+ 
   personajes: Personaje[] = [
-
     {
       nombre: 'Goku',
       poder: 15000
@@ -24,26 +22,10 @@ export class MainPageComponent {
       nombre: 'Vegeta',
       poder: 8500
     }
-
   ];
 
-
-  nuevo: Personaje = {
-    nombre: '',
-    poder: 0
-  }
-
-  agregar( ){
-    if(this.nuevo.nombre.trim().length === 0){
-      return
-    }
-    console.log(this.nuevo);
-
-    this.personajes.push( this.nuevo );
-    this.nuevo = { 
-      nombre:'',
-      poder: 0
-    }
-  }
-
+  nuevo: Personaje ={ 
+      nombre: 'Maestro Roshi',
+      poder: 1000
+  };
 }
