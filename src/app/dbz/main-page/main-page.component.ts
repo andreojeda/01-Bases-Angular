337dc4a714e91dda43d599ec324f4293a2149007
Andres Ojeda <carlosandresoj@gmail.com>
2022-12-21 11:41:16 -0500

    Servicios

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
+import { DbzService } from '../services/dbz.service';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -28,7 +30,11 @@ export class MainPageComponent {
       nombre: 'Maestro Roshi',
       poder: 1000
   };
+
   agregarNuevoPersonaje( argumento: Personaje){
     this.personajes.push(argumento);
   }
+
+  constructor( private dbzService: DbzService ) { }
+
 }
